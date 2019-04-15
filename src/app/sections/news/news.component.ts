c8e557e07ea751ff1f00793f7c64bb9b64be0a2b
DanielSerio <dserio.d@gmail.com>
2019-04-15 16:10:11 -0500

    version 1

--- src/app/sections/news/news.component.ts
+++ src/app/sections/news/news.component.ts
@@ -1,19 +1,31 @@
 import { Article } from './../../shared/models/news-response-data';
 import { DataService } from './../../shared/services/data.service';
-import { Component, OnInit } from '@angular/core';
-import { testData } from './test-data';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { timer } from 'rxjs';
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
   styleUrls: ['./news.component.scss']
 })
-export class NewsComponent implements OnInit {
-  public articles: Article[] = testData;
+export class NewsComponent implements OnInit, OnDestroy {
+  public articles: Article[];
+  private timerSub: Subscription;
+  private newsSub: Subscription;
 
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    this.timerSub = timer(0, 350000)
+      .subscribe(n => {
+        this.newsSub = this.dataService.getNewsData().subscribe(data => {
+          this.articles = data.articles;
+        });
+      });
   }
 
+  ngOnDestroy() {
+    this.newsSub.unsubscribe();
+    this.timerSub.unsubscribe();
+  }
 }
--- src/app/sections/weather/weather.component.ts
+++ src/app/sections/weather/weather.component.ts
@@ -1,17 +1,30 @@
+import { Subscription } from 'rxjs';
+import { DataService } from './../../shared/services/data.service';
 import { WeatherResponseData } from './../../shared/models/weather-response-data';
-import { Component, OnInit } from '@angular/core';
-import { weatherTestData } from './test-data';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
   styleUrls: ['./weather.component.scss']
 })
-export class WeatherComponent implements OnInit {
-  public weatherResponse: WeatherResponseData = weatherTestData;
-  constructor() { }
+export class WeatherComponent implements OnInit, OnDestroy {
+  public weatherResponse: WeatherResponseData | null;
+  private timerSub: Subscription;
+  private weatherSub;
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    this.timerSub = timer(0, 200000).subscribe(n => {
+      this.weatherSub = this.dataService.getWeatherData()
+        .subscribe(data => this.weatherResponse = data);
+    });
   }
 
+  ngOnDestroy() {
+    this.timerSub.unsubscribe();
+    this.weatherSub.unsubscribe();
+  }
 }
